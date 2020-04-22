cda63eeac42596b32ba0c58c956d77191b7e261c
yanngH <solaao@163.com>
2020-04-22 16:03:39 +0800

    分成订单列表增加输出到账提示

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -1146,7 +1146,7 @@ class Order extends Base
         $orderLogic = new OrderLogic();
         $return_goods['refund_money'] = $orderLogic->getRefundGoodsMoney($return_goods);
 
-        if ('weixinJsApi' == $order['pay_code'] || 'weixin' == $order['pay_code'] || 'alipay' == $order['pay_code'] || 'alipayMobile' == $order['pay_code']) {
+        if ('weixinJsApi' == $order['pay_code'] || 'weixin' == $order['pay_code'] || 'alipay' == $order['pay_code'] || 'alipayMobile' == $order['pay_code'] || 'alipayApp' == $order['pay_code'] || 'weixinApp' == $order['pay_code']) {
             if ('weixinJsApi' == $order['pay_code']) {
                 include_once PLUGIN_PATH . 'payment/weixinJsApi/weixinJsApi.class.php';
                 $payment_obj = new \weixinJsApi();
