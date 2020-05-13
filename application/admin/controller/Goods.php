e706350acc560b06fbfe1475d9e78c560e21f9d5
刘志华 <1145094033@qq.com>
2020-05-13 16:55:54 +0800

    fix

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -262,7 +262,7 @@ class Goods extends Base
                 $suppliers = M('Suppliers')->where('suppliers_id', $val['suppliers_id'])->getField('suppliers_name');
                 $price = $val['shop_price'] - $val['exchange_integral'];
                 $strTable .= '<tr>';
-                $strTable .= '<td style="text-align:center;font-size:12px; vnd.ms-excel.numberformat:@;">' .$val['goods_sn'] . '</td>';
+                $strTable .= '<td style="text-align:center;font-size:12px; vnd.ms-excel.numberformat:@;">' . $val['goods_sn'] . '</td>';
                 $strTable .= '<td style="text-align:center;font-size:12px;">' . $first_cat . '</td>';
                 $strTable .= '<td style="text-align:left;font-size:12px;">' . $secend_cat . ' </td>';
                 $strTable .= '<td style="text-align:left;font-size:12px;">' . $third_cat . '</td>';
@@ -401,7 +401,7 @@ class Goods extends Base
             }
             $data['virtual_indate'] = !empty($virtual_indate) ? strtotime($virtual_indate) : 0;
             $data['exchange_integral'] = (1 == $data['is_virtual']) ? 0 : $data['exchange_integral'];
-
+            $data['video'] = $data['video_path'];
             //积分按系统默认比例 BY J
             if (1 == $data['exchange_integral_type'] && 1 != $data['is_virtual']) {
                 $data['exchange_integral'] = $data['shop_price'] * tpCache('shopping.point_use_percent') / 100;
@@ -596,7 +596,7 @@ class Goods extends Base
         $this->assign('brandList', $brandList);
         $this->assign('goodsType', $goodsType);
         $this->assign('goodsInfo', $goodsInfo);  // 商品详情
-        $this->assign('video_url',$goodsInfo['video']?\plugins\Oss::url($goodsInfo['video']):'');  // 视频url
+        $this->assign('video_url', $goodsInfo['video'] ? \plugins\Oss::url($goodsInfo['video']) : '');  // 视频url
 
         $goodsImages = M('GoodsImages')->where('goods_id =' . I('GET.id', 0))->select();
         $this->assign('goodsImages', $goodsImages);  // 商品相册
