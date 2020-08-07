c0de458aefbc090b4839552b0cc72d51ad40ee27
yanngH <solaao@163.com>
2020-08-07 11:26:05 +0800

    分享二维码中间添加logo

--- application/home/controller/api/Goods.php
+++ application/home/controller/api/Goods.php
@@ -123,9 +123,12 @@ class Goods extends Base
         }
 
         $filename = 'public/images/qrcode/goods/goods_' . $user_id . '_' . $goods_id . '.png';
-
         if (!file_exists($filename)) {
-            $this->scerweima($user_id, $goods['goods_id']);
+            $logo = 'public/images/qrcode/qr_logo.png';
+            if (!file_exists($logo)) {
+                $logo = '';
+            }
+            $this->scerweima($user_id, $goods['goods_id'], $logo);
         }
 
         //直接输出
@@ -133,7 +136,7 @@ class Goods extends Base
         exit;
     }
 
-    private function scerweima($user_id, $goods_id)
+    private function scerweima($user_id, $goods_id, $logo = '')
     {
         Loader::import('phpqrcode', EXTEND_PATH);
 
@@ -150,6 +153,30 @@ class Goods extends Base
         //生成二维码图片
         $filename = 'public/images/qrcode/goods/goods_' . $user_id . '_' . $goods_id . '.png';
         \QRcode::png($value, $filename, $errorCorrectionLevel, $matrixPointSize, 2);
+
+        //判断是否生成带logo的二维码
+        if (file_exists($logo)) {
+            $QR = imagecreatefromstring(file_get_contents($filename));        //目标图象连接资源。
+            $logo = imagecreatefromstring(file_get_contents($logo));    //源图象连接资源。
+
+            $QR_width = imagesx($QR);            //二维码图片宽度
+            $QR_height = imagesy($QR);            //二维码图片高度
+            $logo_width = imagesx($logo);        //logo图片宽度
+            $logo_height = imagesy($logo);        //logo图片高度
+            $logo_qr_width = $QR_width / 4;       //组合之后logo的宽度(占二维码的1/5)
+            $scale = $logo_width / $logo_qr_width;       //logo的宽度缩放比(本身宽度/组合后的宽度)
+            $logo_qr_height = $logo_height / $scale;  //组合之后logo的高度
+            $from_width = ($QR_width - $logo_qr_width) / 2;   //组合之后logo左上角所在坐标点
+
+            //重新组合图片并调整大小
+            //imagecopyresampled() 将一幅图像(源图象)中的一块正方形区域拷贝到另一个图像中
+            imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+
+            //输出图片
+            imagepng($QR, $filename);
+            imagedestroy($QR);
+            imagedestroy($logo);
+        }
     }
 
     /**
@@ -733,7 +760,11 @@ class Goods extends Base
             // 分享二维码
             $filename = 'public/images/qrcode/goods/goods_' . $this->user_id . '_' . $goods_id . '.png';
             if (!file_exists($baseUrl . $filename)) {
-                $this->scerweima($this->user_id, $goods['goods_id']);
+                $logo = 'public/images/qrcode/qr_logo.png';
+                if (!file_exists($logo)) {
+                    $logo = '';
+                }
+                $this->scerweima($this->user_id, $goods['goods_id'], $logo);
             }
             $result['goods']['qr_code'] = $baseUrl . $filename;
         }
@@ -821,7 +852,11 @@ class Goods extends Base
             // 分享二维码
             $filename = '/public/images/qrcode/goods/goods_' . $this->user_id . '_' . $goods_id . '.png';
             if (!file_exists(SITE_URL . $filename)) {
-                $this->scerweima($this->user_id, $goods['goods_id']);
+                $logo = 'public/images/qrcode/qr_logo.png';
+                if (!file_exists($logo)) {
+                    $logo = '';
+                }
+                $this->scerweima($this->user_id, $goods['goods_id'], $logo);
             }
             $goodsInfo['share_qr_code'] = SITE_URL . $filename;
         } else {
