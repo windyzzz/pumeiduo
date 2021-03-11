a53823f99cf7ed033b7045ed2f50ebcfdb10f86b
yanngH <solaao@163.com>
2021-03-11 14:47:47 +0800

    优化代码

--- application/common/logic/Qrcode.php
+++ application/common/logic/Qrcode.php
@@ -12,11 +12,6 @@ class Qrcode
      * @param $code
      * @param $userId
      * @return array
-     * @throws Exception
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
-     * @throws \think\exception\PDOException
      */
     public function getInfo($code, $userId)
     {
@@ -24,8 +19,9 @@ class Qrcode
             /*
              * 扫码优惠活动
              */
-            $prom = M('prom_qrcode')->where(['code' => $code, 'is_open' => 1, 'is_del' => 0])->find();
+            $prom = M('prom_qrcode')->where(['code' => $code, 'is_del' => 0])->find();
             if (!$prom) throw new Exception('活动不存在');
+            if ($prom['is_open'] != 1) throw new Exception('活动未开启');
             if ($prom['start_time'] > NOW_TIME) throw new Exception('活动未开始');
             if ($prom['end_time'] < NOW_TIME) throw new Exception('活动已结束');
             // 扫码记录判断
@@ -36,8 +32,8 @@ class Qrcode
             $electronic = '0';
             switch ($prom['reward_type']) {
                 case 1:
-                    $couponIds = $prom['reward_content'];
                     // 发放优惠券
+                    $couponIds = $prom['reward_content'];
                     $couponLogic = new CouponLogic();
                     $res = $couponLogic->receive($couponIds, $userId, true);
                     if ($res['status'] == 0) {
@@ -62,6 +58,7 @@ class Qrcode
                     }
                     break;
                 case 2:
+                    // 发放电子币
                     $electronic = $prom['reward_content'];
                     accountLog($userId, 0, 0, '扫码优惠活动奖励', 0, 0, '', $electronic, 26);
                     break;
