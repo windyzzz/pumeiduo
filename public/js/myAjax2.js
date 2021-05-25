da6cd825f91fe46cf71cb3c9d47dc9b265cd3829
yanngH <solaao@163.com>
2021-05-25 17:01:54 +0800

    管理后台 —— 商品列表添加“系统上下架”“运费模板选择”

--- public/js/myAjax2.js
+++ public/js/myAjax2.js
@@ -45,76 +45,77 @@ function del_fun(del_url)
 {
     if(confirm("确定要删除吗?"))
         location.href = del_url;
-}  
+}
 
 
 // 修改指定表的指定字段值 包括有按钮点击切换是否 或者 排序 或者输入框文字
-function changeTableVal(table,id_name,id_value,field,obj)
-{	
-		var src = "";
-		 if($(obj).hasClass('no')) // 图片点击是否操作
-		 {          
-				//src = '/public/images/yes.png';
-				$(obj).removeClass('no').addClass('yes');
-				$(obj).html("<i class='fa fa-check-circle'></i>是");
-				var value = 1;
-				
-		 }else if($(obj).hasClass('yes')){ // 图片点击是否操作                     
-				$(obj).removeClass('yes').addClass('no');
-				$(obj).html("<i class='fa fa-ban'></i>否");
-				var value = 0;
-		 }else{ // 其他输入框操作
-			 var value = $(obj).val();			 
-	     }
-		                                                  
-		$.ajax({
-				url:"/index.php?m=Admin&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,			
-				success: function(data){									
-				     if(!$(obj).hasClass('no') && !$(obj).hasClass('yes'))								
-					 layer.msg('更新成功', {icon: 1});     
-				}
-		});		
+function changeTableVal(table, id_name, id_value, field, obj) {
+	var src = "";
+	var value = "";
+	if ($(obj).hasClass('no')) {
+		// 图片点击是否操作
+		//src = '/public/images/yes.png';
+		$(obj).removeClass('no').addClass('yes');
+		$(obj).html("<i class='fa fa-check-circle'></i>是");
+		value = 1;
+	} else if ($(obj).hasClass('yes')) {
+		// 图片点击是否操作
+		$(obj).removeClass('yes').addClass('no');
+		$(obj).html("<i class='fa fa-ban'></i>否");
+		value = 0;
+	} else {
+		// 其他输入框操作
+		value = $(obj).val();
+	}
+	$.ajax({
+		url: "/index.php?m=Admin&c=Index&a=changeTableVal&table=" + table + "&id_name=" + id_name + "&id_value=" + id_value + "&field=" + field + '&value=' + value,
+		success: function (data) {
+			if (!$(obj).hasClass('no') && !$(obj).hasClass('yes')) {
+				layer.msg('更新成功', {icon: 1});
+			}
+		}
+	});
 }
 
 // 给商家用的修改某个表的某个字段  商家是需要判断当前session的
 // 修改指定表的指定字段值 包括有按钮点击切换是否 或者 排序 或者输入框文字
 function changeTableVal2(table,id_name,id_value,field,obj)
-{	
+{
 		var src = "";
 		 if($(obj).hasClass('no')) // 图片点击是否操作
-		 {          
+		 {
 				//src = '/public/images/yes.png';
 				$(obj).removeClass('no').addClass('yes');
 				$(obj).html("<i class='fa fa-check-circle'></i>是");
 				var value = 1;
-				
-		 }else if($(obj).hasClass('yes')){ // 图片点击是否操作                     
+
+		 }else if($(obj).hasClass('yes')){ // 图片点击是否操作
 				$(obj).removeClass('yes').addClass('no');
 				$(obj).html("<i class='fa fa-ban'></i>否");
 				var value = 0;
 		 }else{ // 其他输入框操作
-			 var value = $(obj).val();			 
+			 var value = $(obj).val();
 	     }
-		                                                  
+
 		$.ajax({
-				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,			
-				success: function(data){	
-				     if(!$(obj).hasClass('no') && !$(obj).hasClass('yes'))								
+				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,
+				success: function(data){
+				     if(!$(obj).hasClass('no') && !$(obj).hasClass('yes'))
 					 layer.msg('更新成功', {icon: 1});
 				}
-		});		
+		});
 }
 /*
 // 修改指定表的排序字段
 function updateSort(table,id_name,id_value,field,obj)
-{		       
+{
  		var value = $(obj).val();
 		$.ajax({
-				url:"/index.php?m=Admin&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,			
-				success: function(data){									
-					layer.msg('更新成功', {icon: 1});   
+				url:"/index.php?m=Admin&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,
+				success: function(data){
+					layer.msg('更新成功', {icon: 1});
 				}
-		});		
+		});
 }
 
 // 修改指定表的指定字段值 给商家使用的函数
@@ -122,32 +123,31 @@ function changeTableVal2(table,id_name,id_value,field,obj)
 {
 		var src = "";
 		 if($(obj).attr('src').indexOf("cancel.png") > 0 )
-		 {          
+		 {
 				src = '/public/images/yes.png';
 				var value = 1;
-				
-		 }else{                    
+
+		 }else{
 				src = '/public/images/cancel.png';
 				var value = 0;
-		 }                                                 
+		 }
 		$.ajax({
-				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,			
-				success: function(data){									
-					$(obj).attr('src',src);           
+				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,
+				success: function(data){
+					$(obj).attr('src',src);
 				}
-		});		
+		});
 }
 
 // 修改指定表的排序字段 给商家使用的函数
 function updateSort2(table,id_name,id_value,field,obj)
-{		       
+{
  		var value = $(obj).val();
 		$.ajax({
-				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,			
-				success: function(data){									
-					layer.msg('更新成功', {icon: 1});   
+				url:"/index.php?m=Seller&c=Index&a=changeTableVal&table="+table+"&id_name="+id_name+"&id_value="+id_value+"&field="+field+'&value='+value,
+				success: function(data){
+					layer.msg('更新成功', {icon: 1});
 				}
-		});		
-} 
+		});
+}
 */
- 
