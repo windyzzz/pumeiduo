43d724e59d353067653f9bbd18652954b6cbf74f
Iayoo <vilnius94@163.com>
2021-12-09 10:00:32 +0800

    # 新增 推荐人变更逻辑修改

--- application/common/logic/user/ReferrerLogic.php
+++ application/common/logic/user/ReferrerLogic.php
@@ -36,5 +36,41 @@ class ReferrerLogic
         if ((integer)$userInfo['first_leader'] !== (integer)$old){
             return ['status' => 0, 'msg' => '用户上级信息不一致！'];
         }
+
+        $newUserInfo = Db::name('users')->where('user_id',$new)->find();
+
+        if (!$newUserInfo){
+            return ['status' => 0, 'msg' => '用户不存在'];
+        }
+        Db::transaction(function () use ($userInfo,$newUserInfo){
+            $updateData = [
+                'first_leader'   => $newUserInfo['user_id'],
+                'second_leader'  => $newUserInfo['first_leader'],
+                'third_leader'   => $newUserInfo['second_leader'],
+                'referrer_chain' => $newUserInfo['referrer_chain'] . $newUserInfo['user_id'] . ",",
+            ];
+            $updateUserRes = Db::name("users")->where('user_id',$userInfo['user_id'])->update($updateData);
+
+            if ($updateUserRes){
+                throw new \Exception('修改会员信息失败');
+            }
+
+            $updateFirstChildRes = Db::name('users')->where('first_leader',$userInfo['user_id'])->update([
+                'second_leader' => $newUserInfo['user_id'],
+                'third_leader'  => $newUserInfo['first_leader'],
+            ]);
+
+            $updateSecondChildRes = Db::name('users')->where('second_leader',$userInfo['user_id'])->update([
+                'third_leader'  => $newUserInfo['first_leader'],
+            ]);
+
+            $table = Db::name('users')->getTable();
+            $updateChildChainRes = Db::execute("UPDATE `{$table}` SET `referrer_chain` = REPLACE(referrer_chain,'{$userInfo['referrer_chain']}{$userInfo['user_id']},','{$newUserInfo['referrer_chain']}{$newUserInfo['user_id']},{$userInfo['user_id']},') WHERE `referrer_chain` LIKE '%{$userInfo['referrer_chain']}{$userInfo['user_id']},'");
+
+            if (($updateFirstChildRes || $updateSecondChildRes) && !$updateChildChainRes){
+                throw new \Exception('更新失败');
+            }
+        });
+
     }
 }
