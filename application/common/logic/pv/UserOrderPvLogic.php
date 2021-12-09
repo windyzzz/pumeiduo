e0ee77ec4ddc8c5d0dbdc7d2b4ef79062674b992
Iayoo <vilnius94@163.com>
2021-12-09 14:45:08 +0800

    # 新增 月结逻辑修改 新增经销商辅导费

--- application/common/logic/pv/UserOrderPvLogic.php
+++ application/common/logic/pv/UserOrderPvLogic.php
@@ -2,34 +2,78 @@
 
 namespace app\common\logic\pv;
 
+use think\Collection;
 use think\Db;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
+use think\exception\DbException;
 
 class UserOrderPvLogic
 {
-    public function getPvByUserLevel($userLevel,$year = null,$month = null, $day = null){
+    protected $where = [];
 
-        $group = ['chain_user_id'];
-        $where = [
-            'level' => $userLevel
-        ];
+    protected $group = [];
 
+    public function setDate($year = null,$month = null, $day = null){
         if (!empty($year)){
-            $group[] = 'year';
-            $where['year'] = $day;
+            $this->group[] = 'year';
+            $this->where['year'] = $day;
         }
         if (!empty($month)){
-            $group[] = 'month';
-            $where['month'] = $day;
+            $this->group[] = 'month';
+            $this->where['month'] = $day;
         }
         if (!empty($day)){
-            $group[] = 'day';
-            $where['day'] = $day;
+            $this->group[] = 'day';
+            $this->where['day'] = $day;
         }
+        return $this;
+    }
 
+    /**
+     * 根据用户等级获取所有符合等级的用户小组业绩
+     * @param $userLevel
+     * @return bool|false|\PDOStatement|string|Collection
+     * @throws DataNotFoundException
+     * @throws ModelNotFoundException
+     * @throws DbException
+     */
+    public function getPvByUserLevel($userLevel){
+
+        $group = array_merge(['chain_user_id'],$this->group);
+
+        $where = array_merge([
+            'level' => $userLevel
+        ],$this->where);
+
+        return Db::name("users u")->join('user_order_pv_log pv','u.user_id=pv.chain_user_id','LEFT')
+            ->group(implode(",",$group))
+            ->where($where)
+            ->where('chain_user_generation','<=',1)
+            ->field("u.user_id,pv.year,pv.month,pv.day,SUM(pv.pv) total_pv,SUM(pv.team_pv) total_team_pv")
+            ->select();
+    }
+
+    public function getPvByUserChainNumber($chainNumber){
+        $group = array_merge(['chain_user_id'],$this->group);
+
+        $where = array_merge([
+            'chain_user_generation' => $chainNumber
+        ],$this->where);
         return Db::name("users u")->join('user_order_pv_log pv','u.user_id=pv.chain_user_id','LEFT')
             ->group(implode(",",$group))
             ->where($where)
             ->field("u.user_id,pv.year,pv.month,pv.day,SUM(pv.pv) total_pv,SUM(pv.team_pv) total_team_pv")
             ->select();
+    }
+
+    /**
+     * 查询个人业绩
+     * @param $userId
+     * @return bool|float|int|string|null
+     */
+    public function getUserPv($userId){
+        $where = array_merge(['chain_user_id'=>$userId,'chain_user_generation'=>0],$this->where);
+        return Db::name('user_order_pv_log')->where($where)->sum('pv');
     }
 }
--- application/home/command/shop/MonthlyBonus.php
+++ application/home/command/shop/MonthlyBonus.php
@@ -7,6 +7,9 @@ namespace app\home\command\shop;
 use app\common\logic\pv\UserOrderPvLogic;
 use think\console\Input;
 use think\console\Output;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\ModelNotFoundException;
+use think\exception\DbException;
 
 class MonthlyBonus extends BaseCommand
 {
@@ -15,47 +18,67 @@ class MonthlyBonus extends BaseCommand
         $this->setName('monthly_bonus')->setDescription('月结奖金');
     }
 
+    /**
+     * @param Input $input
+     * @param Output $output
+     * @return int|void|null
+     * @throws DataNotFoundException
+     * @throws DbException
+     * @throws ModelNotFoundException
+     */
     protected function execute(Input $input, Output $output)
     {
         $start = microtime(true);
         $output->writeln('开始处理：' . date('Y-m-d H:i:s'));
         // 更新商品信息（excel文件导入）
-        $this->publicityAndEducation();
+        $this->getMonthlyPv();
         $output->writeln('程序结束：' . date('Y-m-d H:i:s'));
         $end = microtime(true);
         $output->writeln('所用时间：' . bcsub($end, $start, 5));
     }
 
     /**
-     * 宣传教育费
+     * @throws DataNotFoundException
+     * @throws ModelNotFoundException
+     * @throws DbException
      */
-    protected function publicityAndEducation(){
+    protected function getMonthlyPv(){
         $pvLogic = new UserOrderPvLogic();
         $month = date("Y-m",strtotime(date("Y-m",$this->time) . " -1 month"));
-        $userPvLog = $pvLogic->getPvByUserLevel(1,null,$month);
+        $pvLogic->setDate(null,$month);
+
+        $userPvLog = $pvLogic->getPvByUserLevel(1);
+        $secondUserPvLog = $pvLogic->getPvByUserChainNumber(2);
         foreach ($userPvLog as $item){
             if ($item['total_pv'] >= 300 && $item['total_team_pv'] >= 1000){
-                $sendBonus = bcmul($item['total_team_pv'],bcdiv('8','100',2),2);
-                $this->output->writeln("用户：「{$item['user_id']}」获得宣传教育费 {$sendBonus}");
+                $this->publicityAndEducation($item['user_id'],$item['total_team_pv']);
+                $this->dealerGuidance($item['user_id'],$secondUserPvLog);
             }else{
                 continue;
             }
         }
+
+    }
+
+    /**
+     * 宣传教育费
+     * @param $userId
+     * @param $teamPv
+     */
+    protected function publicityAndEducation($userId,$teamPv){
+        $sendBonus = bcmul($teamPv,bcdiv('8','100',2),2);
+        $this->output->writeln("用户：「{$userId}」获得宣传教育费 {$sendBonus}");
     }
 
     /**
      * 经销商辅导费
      */
-    protected function dealerGuidance(){
-        $pvLogic = new UserOrderPvLogic();
-        $month = date("Y-m",strtotime(date("Y-m",$this->time) . " -1 month"));
-        $userPvLog = $pvLogic->getPvByUserLevel(1,null,$month);
-        foreach ($userPvLog as $item){
-            if ($item['total_pv'] >= 300 && $item['total_team_pv'] >= 1000){
-                $sendBonus = bcmul($item['total_team_pv'],bcdiv('8','100',2),2);
-                $this->output->writeln("用户：「{$item['user_id']}」获得宣传教育费 {$sendBonus}");
-            }else{
-                continue;
+    protected function dealerGuidance($userId,$secondUserPvLog){
+        foreach ($secondUserPvLog as $item){
+            if ($item['user_id'] === $userId){
+                $sendBonus = bcmul($item['total_team_pv'],bcdiv('3','100',2),2);
+                $this->output->writeln("用户：「{$userId}」获得经销商辅导费 {$sendBonus}");
+                return ;
             }
         }
     }
